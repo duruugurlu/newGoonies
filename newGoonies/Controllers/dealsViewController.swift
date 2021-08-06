bf0df96760a0a6ac5556dd52832f5df98329c81a
Duru Ugurlu <duru.ugurlu@tufts.edu>
2021-08-06 14:12:33 +0300

    deals page done(ish)

--- newGoonies/Controllers/dealsViewController.swift
+++ newGoonies/Controllers/dealsViewController.swift
@@ -9,22 +9,64 @@ import UIKit
 
 class dealsViewController: UIViewController {
 
+    var itemNum = 1
+    @IBOutlet weak var materialsLabel: UILabel!
+    @IBOutlet weak var materialsContentLabel: UILabel!
+    @IBOutlet weak var instructionsLabel: UILabel!
+    @IBOutlet weak var instructionstContentLabel: UILabel!
+    
+    @IBOutlet weak var descriptionsLabel: UILabel!
+    @IBOutlet weak var quantLabel: UILabel!
+    
+    @IBOutlet weak var itemsLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var quantityLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        quantityLabel.font = UIFont(name: "Poppins-Bold", size: 15.0)
-        // Do any additional setup after loading the view.
+        configureUI()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureUI() {
+        quantityLabel.font = UIFont(name: "Poppins-Bold", size: 15.0)
+        itemsLabel.font = UIFont(name: "Poppins-Light", size: 9.0)
+        totalLabel.font = UIFont(name: "Poppins-SemiBold", size: 17.0)
+        priceLabel.font = UIFont(name: "Poppins-Bold", size: 24.0)
+        materialsLabel.font = UIFont(name: "Poppins-Bold", size: 15.0)
+        materialsContentLabel.font = UIFont(name: "Poppins-Light", size: 12.0)
+        instructionsLabel.font = UIFont(name: "Poppins-Bold", size: 15.0)
+        instructionstContentLabel.font = UIFont(name: "Poppins-Light", size: 12.0)
+        descriptionsLabel.font = UIFont(name: "Poppins-Bold", size: 15.0)
+        quantLabel.font = UIFont(name: "Poppins-Bold", size: 15.0)
+        configureQuantity()
     }
-    */
-
+    
+    func configureQuantity () {
+        quantityLabel.text = "\(itemNum)"
+        if itemNum != 1 {
+            itemsLabel.text = "\(itemNum) items"
+        } else {
+            itemsLabel.text = "\(itemNum) item"
+        }
+       
+        priceLabel.text = "$ \(17 * itemNum)"
+    }
+    
+    @IBAction func decreasePressed(_ sender: Any) {
+        if itemNum != 1 {
+            itemNum = itemNum - 1
+            configureQuantity()
+        }
+    }
+    
+    @IBAction func increasePressed(_ sender: Any) {
+        itemNum = itemNum + 1
+        configureQuantity()
+    }
+    
+    @IBAction func homePressed(_ sender: Any) {
+        performSegue(withIdentifier: "dealsToMain", sender: nil)
+    }
+    
 }
