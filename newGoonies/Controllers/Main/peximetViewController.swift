0e6496a28fa81f0fae213ebd576a3adc8d63ea1f
Duru Ugurlu <duru.ugurlu@tufts.edu>
2021-08-16 08:57:55 +0300

    connection to the restaurant specific page + start of the segmented control

--- newGoonies/Controllers/Main/peximetViewController.swift
+++ newGoonies/Controllers/Main/peximetViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class peximetViewController: UIViewController {
 
+    @IBOutlet weak var pexSegControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
-
+    
+    @IBAction func indexChanged(_ sender: Any) {
+        switch pexSegControl.selectedSegmentIndex {
+            case 0 : print("index 0")
+            case 1 : print("index 1")
+            case 2 : print("index 2")
+            default : break
+        }
+    }
+    
 }
